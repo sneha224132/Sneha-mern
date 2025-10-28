e4840df4f3595ab0a798f6424b25c9821932acdb
Sneha Jijo <sneha224132@sahrdaya.ac.in>
2025-10-28 22:15:58 +0530

    Update Book.js

--- Book.js
+++ Book.js
@@ -1,13 +1,33 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: { type: String, required: true },
-  price: { type: Number, required: true },
-  stock: { type: Number, default: 0 },
-  publishedYear: { type: Number },
-  createdAt: { type: Date, default: Date.now }
+  title: { 
+    type: String, 
+    required: true 
+  },
+  author: { 
+    type: String, 
+    required: true 
+  },
+  genre: { 
+    type: String, 
+    required: true 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  stock: { 
+    type: Number, 
+    default: 0 
+  },
+  publishedYear: { 
+    type: Number 
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
-module.exports = mongoose.model('Book', bookSchema);
+export const Book = mongoose.model('Book', bookSchema);
